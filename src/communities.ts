bc4bb84c14a30e3c223cd1c08b304bc9a6a21b25
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-03-24 07:15:23 -0400

    Add communities

---
+++ src/communities.ts
@@ -0,0 +1,15 @@
+export const communities: string[] = [
+	"STEM",
+	"Humanities and Social Sciences",
+	"Professional Studies",
+	"Arts and Design",
+	"Environmental and Agricultural Sciences",
+	"Creative and Performing Arts",
+	"Sports and Recreation",
+	"Spritual and Religious",
+	"Social and Lifestyle",
+	"Career Services and Networking",
+	"Student Governance and Leadership",
+	"Wellness and Mental Health",
+	"Diversity and Inclusion",
+]
